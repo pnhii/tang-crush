ca4394338126ebadff239f977effb42e5c0cef7c
pnhii <85268081+pnhii@users.noreply.github.com>
2021-06-03 11:11:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hehe',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'vẫn phải chọn pnhi',
+    btnNo: 'Khom del chọn đou',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mài thỉu năng',
+    btnReply: 'Gửi cho pnhi nè',
+    reply: 'ai bít tại dì teo thỉu năng',
+    mess: 'trên báo đưa tin mài thỉu năng lémm',
+    messDesc: 'nên là hết dịch mềnh đi chơi nè',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
